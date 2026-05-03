17c93b06e90061f6d8e46850bbbc28b2a625ee51
Akbar-AI97 <akbaratakhanov@gmail.com>
2026-05-03 13:47:23 +0900

    feat: integrate meta tags on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,26 @@ export default function Document() {
   console.log("DOCUMENT - PAGES ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/* CEO - search engine optimization */}
+        <meta
+          name="keyword"
+          content="nestar, nestar.uz, devex mern, mern nestjs fullstack"
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere anytime in South Korea. | " +
+            "Покупайте и продавайте недвижимость в любом месте и в любое время в Южной Корее. | " +
+            "대한민국 언제 어디서나 부동산을 사고팔 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
