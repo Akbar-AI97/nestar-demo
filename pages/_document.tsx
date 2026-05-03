4935772ec151bc61951e6a531f1783a2b4f3ad9f
Akbar-AI97 <akbaratakhanov@gmail.com>
2026-05-03 13:16:45 +0900

    BRR: initial setup & orginize and test pages router system

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
+// SERVER SIDE RENDERING DOCUMENT
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  console.log("DOCUMENT - PAGES ROUTER");
   return (
     <Html lang="en">
       <Head />
---
+++ pages/property/index.tsx
@@ -0,0 +1,7 @@
+// BROWSER COMPONENT
+const PropertyList = () => {
+  console.log("PROPERTY LIST COMPONENT - PAGES ROUTER"); // logs in browser's console
+  return <div>PROPERTY LIST</div>;
+};
+
+export default PropertyList;
